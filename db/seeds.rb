3fc4bfde1f9fde458d447303a264ec8c6b37b706
Michael Hicks <hicks.michael04@gmail.com>
2023-11-09 08:40:30 -0700

    Iterate through products on index view to show all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 Product.delete_all
 
-Product.create!(title: "Pocket Pills", description: "The best way to get your dog to take their medication by tucking it away in a tasty treat!", image_url: 'dpchat.jpg', price: 20.00)
+Product.create!(title: "Foot Binoculars", description: "Creep around with your feet, not your hands!", image_url: 'dpchat.jpg', price: 20.00)
+Product.create!(title: "Trip to Vail", description: "Beautiful mountain town weekend getaway", image_url: 'vail.jpg', price: 250.00)
+Product.create!(title: "Tom Drum", description: "Not quite a snare drum, but not exaclty a kick drum.", image_url: 'tom1.png', price: 300.00)
