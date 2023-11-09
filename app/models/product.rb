06f050de2e795155b7c02df355491344bc01a292
Michael Hicks <hicks.michael04@gmail.com>
2023-11-09 10:26:28 -0700

    Add validations for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :description, presence: true
+    validates :title, uniqueness: true, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }, presence: true
+    validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif|jpg|jpeg|png)\z}i,
+        message: 'must be a URL for GIF, JPG, JPEG, or PNG image.'
+    }
 end
